2afa1aa372c619ff8d338db6a051ad22ac9e07cf
Younus Ali Akash <younusaliakash8@gmail.com>
2021-03-02 20:39:40 +0600

    js dom

---
+++ Selector.js
@@ -0,0 +1,31 @@
+// var title = document.getElementById('title')
+// console.log(title)
+// var par = document.getElementsByClassName('lead')
+// console.log(par)
+
+// let list = document.getElementsByTagName('li')
+// console.log(list)
+
+// let names = document.getElementsByName('list-item-one')
+// console.log(names)
+
+// let title = document.querySelector('#title')
+// console.log(title)
+
+// let paragraph = document.querySelectorAll ('.lead')
+// console.log(paragraph)
+
+// let list = document.querySelectorAll('li')
+// console.log(list)
+
+// let listitem = document.querySelector('[name^ = ""]')
+// console.log(listitem)
+
+// let list1 = document.getElementsByTagName('li')
+// console.log(list1)
+
+// let list2 = document.querySelectorAll('li')
+// console.log(list2)
+
+// console.log(list1.constructor.name)
+// console.log(list2.constructor.name)
---
+++ travers.js
@@ -0,0 +1,20 @@
+// let list = document.getElementById('list')
+
+// let parent = list.parentElement
+// console.log(parent)
+
+// let childern = list.children
+// console.log(childern)
+
+// let listItem = document.getElementsByTagName('li')
+// console.log(listItem)
+
+// let listItems = Array.from(listItem)
+// let listItems = Array.prototype.slice.apply(listItem)
+// let listItems  = [...listItem]
+// // console.log(listItems)
+
+// listItems.forEach((li, ind) => {
+//     let text = li.innerHTML
+//     li.innerHTML = `${ind +1 } . ${text}` 
+// })
